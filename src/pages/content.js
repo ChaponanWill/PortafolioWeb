16a0153f08e3e78d30958d6c1fc9082ec6bb6902
chaponanwill <chapoeilliam09@gmail.com>
2024-03-12 03:27:46 -0500

    portafolioI

--- src/pages/content.js
+++ src/pages/content.js
@@ -1,92 +1,85 @@
 // Personal Information --> #hero section
-export const name = "Peter Griffin"
-export const designation = "Safety Inspector"
-export const location = "Quahog, Rhode Island"
-export const pronouns = "he/him"
-export const website = "https://petergriffin.com"
+export const name = "William Chapoñan"
+export const designation = "Desarrollador de software"
+export const location = "Lambayeque, Perú"
+export const pronouns = "ChaponanWill"
+export const website = "https://github.com/ChaponanWill"
 
 // About
-export const about = "An adventurous man working as a safety inspector with a quirky sense of humor. Husband to Louis Griffin. Father of Chris, Brian, and Stewie."
+export const about = "Desarrollo software con el objetivo de crear soluciones innovadoras y útiles. Tengo experiencia en diversos lenguajes de programación y tecnologías, y siempre estoy dispuesto a aprender cosas nuevas."
 
 // Work Experience --> #work section
 export const work = [
 	{
-		from: "2001",
-		to: "Present",
-		role: "Safety Inspector",
-		org: "Quahog Safety Department",
-		org_url: "https://quahog.gov/safety",
-		location: "Quahog, Rhode Island",
-		skills: "Safety Inspection, Risk Assessment, Emergency Response"
-	}
+		from: "2023",
+		to: "Reciente",
+		role: "Técnico en informática",
+		org: "Área de imagen institucional e informática",
+		org_url: "https://munipacora.gob.pe",
+		location: "Pacora - Lambayeque",
+		skills: "Redes - Desarrollo de software - Diseño "
+	},
 ]
 
 // Projects --> #project section
 export const projects = [
 	{
-		from: "2023",
-		to: "Present",
-		title: "Project Griffin",
-		description: "A project to improve safety standards in Quahog by implementing innovative safety measures.",
-		url: "https://projectgriffin.com",
-		tech: "Safety Equipment, Training Programs, Community Outreach",
+		from: "2024",
+		to: "Reciente",
+		title: "Crud con PHP + TailwindCss",
+		description: "Mejorar mis habilidades realizando un crud con incio de sesión en PHP",
+		url: "https://asistenciacrudfriends.000webhostapp.com/index.html",
+		tech: "Crud de asistencia",
 	},
 	{
 		from: "2023",
-		to: "2022",
-		title: "Family Safety Campaign",
-		description: "An initiative to promote safety awareness among families in Quahog.",
-		url: "https://safefamilyquahog.com",
-		tech: "Workshops, Educational Material, Public Events",
+		to: "Reciente",
+		title: "Página Web - Postrecitos",
+		description: "Página web estática de Postrecitos utilizando tecnologías como HTML, Css y Bootstrap",
+		url: "https://chaponanwill.github.io/postrecitos.github.io/",
+		tech: "Página web",
 	},
-	{
-		from: "2020",
-		to: "2021",
-		title: "Road Safety Initiative",
-		description: "A campaign to enhance road safety and reduce accidents in Quahog.",
-		url: "https://roadsafetyquahog.com",
-		tech: "Traffic Analysis, Signage Improvement, Public Awareness",
-	}
+	
 ]
 
 // Education --> #education section
 export const education = [
 	{
-		from: "1995",
-		to: "1999",
-		institute: "Quahog High School",
-		course: "High School Diploma",
-		location: "Quahog, Rhode Island"
+		from: "2009",
+		to: "2014",
+		institute: "I.E 10927",
+		course: "Primaria",
+		location: "Cerro Escute - Pacora"
 	},
 	{
-		from: "1999",
-		to: "2003",
-		institute: "Quahog Community College",
-		course: "Bachelor of Science in Safety Management",
-		location: "Quahog, Rhode Island"
+		from: "2015",
+		to: "2019",
+		institute: "I.E San Pablo",
+		course: "Secundaria",
+		location: "Pacora - Lambayeque"
 	},
 	{
-		from: "2003",
-		to: "2005",
-		institute: "Safety University",
-		course: "Master of Science in Safety Engineering",
-		location: "Boston, Massachusetts"
+		from: "2021",
+		to: "2024",
+		institute: "SENATI",
+		course: "Ingeniería de Software con Inteligencia Artificial",
+		location: "Chiclayo-Perú"
 	}
 ]
 
 // Contact --> #contact section
 export const contact = [
 	{
-		source_name: "Email",
-		source: "peter.griffin@example.com",
+		source_name: "Facebook",
+		source: "https://facebook.com/ChaponanWill",
 	},
 	{
 		source_name: "Twitter",
-		source: "https://twitter.com/PeterGriffin",
+		source: "https://twitter.com/ChaponanWill",
 	},
 	{
 		source_name: "LinkedIn",
-		source: "https://www.linkedin.com/in/peter-griffin",
+		source: "https://www.linkedin.com/in/ChaponanWill",
 	},
 ]
 
@@ -94,66 +87,24 @@ export const contact = [
 
 export const certificates = [
 	{
-		issued: "2005",
-		title: "Certified Safety Inspector",
-		org: "National Safety Association",
-		url: "https://certi.com/url",
-		description: "Certification in safety inspection and risk assessment.",
+		issued: "2023",
+		title: "Próximo a subir",
+		org: "Próximo",
+		url: "#",
+		description: "Certificado próximo a subir.",
 	},
-	{
-		issued: "2006",
-		title: "Emergency Response Training",
-		org: "Quahog Safety Department",
-		url: "https://certi.com/url",
-		description: "Training in handling emergency situations and crisis management.",
-	},
-	{
-		issued: "2007",
-		title: "Safety Leadership Workshop",
-		org: "Safety Leaders International",
-		url: "https://certi.com/url",
-		description: "Leadership training in safety management and communication.",
-	}
+	// Duplicar más
 ]
 
 // Blogs --> #blogs section
 export const blogs = [
 	{
-		date: "12-July, 2023",
-		url: "https://medium.com/",
-		title: "Adventures in Safety Inspection",
-		description: "Join me in my humorous escapades as a safety inspector in Quahog.",
-		publisher: "Quahog Times",
+		date: "Próxima fecha",
+		url: "#",
+		title: "Blog en Proceso de subir",
+		description: "Muy pronto verás blogs 🚀",
+		// publisher: "Quahog Times", //Descripción de URL
 	},
-	{
-		date: "25-June, 2023",
-		url: "https://medium.com/",
-		title: "Top 10 Safety Tips for Families",
-		description: "Learn the best safety practices for families to ensure a secure environment.",
-		publisher: "Safety Monthly",
-	}
-]
-
-// Achievements --> #achievement section
-export const achievements = [
-	{
-		title: "Safety Hero Award",
-		year: "2020",
-		decription: "Recognized for outstanding contributions to improving safety standards in Quahog."
-	},
-	{
-		title: "Safety Innovation Prize",
-		year: "2018",
-		decription: "Awarded for implementing innovative safety measures that yielded significant improvements."
-	},
-	{
-		title: "Community Safety Ambassador",
-		year: "2015",
-		decription: "Honored for promoting safety awareness and education in the Quahog community."
-	},
-	{
-		title: "Safety Campaign Excellence",
-		year: "2013",
-		decription: "Acknowledged for leading a successful safety campaign that reached thousands of residents."
-	}
+	// Más ...
+	
 ]
