f784491d15bb6e4c9acfe734eb7ef1609ab27521
chaponanwill <chapowilliam09@gmail.com>
2024-04-23 01:04:44 -0500

    pages to

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import alpinejs from "@astrojs/alpinejs";
 export default defineConfig({
   integrations: [tailwind(), alpinejs()],
   output: 'static',
-  site: 'https://ChaponanWill/portolio-web.github.io',
+  site: 'https://ChaponanWill/portfolio-web.github.io',
 });
